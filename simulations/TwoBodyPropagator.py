Fixed two body propagator, added orbital elements calculation (still work in progress) and plotting function

simulations/TwoBodyPropagator.py
@@ -1,12 +1,14 @@
 import numpy as np
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
+from utils.GetClassicOrbitalElements import *
+from utils.visualization import plot_classic_orbital_elements
 
 
 r = np.array([10016.34, -17012.52, 7899.28])
 v = np.array([2.5, -1.05, 3.88])
-t = np.linspace(0, 20000, 100000)
-earth_radius = 6371.0  # in km
+t = np.linspace(0, 60000, 100000)
+earth_radius = 6378.0  # in km
 mu = 3.986e5
 
 def x_dot(t, x):
@@ -14,14 +16,19 @@ def x_dot(t, x):
     xdot[0] = x[3]
     xdot[1] = x[4]
     xdot[2] = x[5]
-    xdot[3:5] = -(mu/np.linalg.norm(x[0:2])**3)*x[0:2]
+    xdot[3:6] = -(mu/(np.linalg.norm(x[0:3]))**3)*x[0:3]
 
     return xdot
 
-x0 = np.concatenate((r, v))
-sol = solve_ivp(x_dot, (t[0], t[-1]), x0, t_eval=t, method='Radau')
+x0 = np.concatenate((r, v)) 
+sol = solve_ivp(x_dot, (t[0], t[-1]), x0, t_eval=t, method='RK45')
 
-X = sol.y[0:2, :]
+X = sol.y
+
+orbital_elementss = []
+for i in range(X.shape[1]):
+    x = X[:, i]
+    orbital_elementss.append(get_orbital_elements(x[0:3], x[3:6], mu))
 
 plt.figure()
 ax = plt.axes(projection='3d')
@@ -30,10 +37,12 @@ x = earth_radius * np.cos(u)*np.sin(v)
 y = earth_radius * np.sin(u)*np.sin(v)
 z = earth_radius * np.cos(v)
 ax.plot_wireframe(x, y, z, color="r")
-ax.plot3D(X[0, :], X[1, :], np.zeros_like(X[0, :]), 'b-')
+ax.plot3D(X[0, :], X[1, :], X[2, :], 'b-')
 ax.set_title('Orbit Propagation')
 ax.axis('equal')
 plt.show()
+
+plot_classic_orbital_elements(t, orbital_elementss)
 
 
 

simulations/utils/GetClassicOrbitalElements.py
@@ -1,5 +1,6 @@
 import numpy as np
 from dataclasses import dataclass
+from .constants import Bases
 
 
 @dataclass
@@ -11,16 +12,24 @@ class OrbitalElements:
     argument_of_perigee: float
     true_anomaly: float
 
-@dataclass
-class Bases(frozen=True):
-    i: np.typing.NDArray = np.array([1, 0, 0]) 
-    j: np.typing.NDArray = np.array([0, 1, 0]) 
-    k: np.typing.NDArray = np.array([0, 0, 1]) 
+
+def get_orbital_elements(r: np.typing.NDArray, v: np.typing.NDArray, mu: float) -> OrbitalElements:
+    orbital_elements = OrbitalElements(
+        major_axis = get_major_axis(r, v, mu),
+        eccentricity = get_eccentricity(r, v, mu),
+        inclination = get_inclination(r, v, mu),
+        ascending_node = get_ascending_node(r, v, mu),
+        argument_of_perigee = get_argument_of_perigee(r, v, mu),
+        true_anomaly = get_true_anormaly(r, v, mu)
+    )
+
+    return orbital_elements
 
 
 def get_major_axis(r: np.typing.NDArray, v: np.typing.NDArray, mu: float) -> float:
     r_norm = np.linalg.norm(r)
-    eps = (r_norm^2)/2 - (mu/r_norm)
+    v_norm = np.linalg.norm(v)
+    eps = (v_norm**2)/2 - (mu/r_norm)
     a = -mu/(2*eps)
 
     return a
@@ -28,7 +37,7 @@ def get_major_axis(r: np.typing.NDArray, v: np.typing.NDArray, mu: float) -> flo
 
 def get_eccentricity(r: np.typing.NDArray, v: np.typing.NDArray, mu: float) -> float:
     e = get_eccentricity_vector(r, v, mu)
-    e_norm = np.linalg.norm(e_norm)
+    e_norm = np.linalg.norm(e)
 
     return e_norm
 