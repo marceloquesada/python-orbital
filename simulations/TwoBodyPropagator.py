c0a45ceee179ce1e7f40b34605aa6f81ed37d265
ppedrogz <ppggonzalez@hotmail.com>
2025-08-28 19:48:43 -0300

    Update TwoBodyPropagator

--- simulations/TwoBodyPropagator.py
+++ simulations/TwoBodyPropagator.py
@@ -7,16 +7,18 @@ from utils.visualization import plot_classic_orbital_elements
 
 r = np.array([10016.34, -17012.52, 7899.28])
 v = np.array([2.5, -1.05, 3.88])
-t = np.linspace(0, 60000, 100000)
+t = np.linspace(0, 200000, 1000000)
 earth_radius = 6378.0  # in km
 mu = 3.986e5
-
+thrust = 1.1e-3
 def x_dot(t, x):
     xdot = np.zeros_like(x)
     xdot[0] = x[3]
     xdot[1] = x[4]
     xdot[2] = x[5]
-    xdot[3:6] = -(mu/(np.linalg.norm(x[0:3]))**3)*x[0:3]
+    h= np.cross(x[0:3], x[3:6])
+    h_hat=np.linalg.norm(h)
+    xdot[3:6] = -(mu/(np.linalg.norm(x[0:3]))**3)*x[0:3] + (thrust*np.linalg.norm(xdot[0:3]))
 
     return xdot
 
@@ -43,8 +45,6 @@ ax.axis('equal')
 plt.show()
 
 plot_classic_orbital_elements(t, orbital_elementss)
-
-
 
 
 
