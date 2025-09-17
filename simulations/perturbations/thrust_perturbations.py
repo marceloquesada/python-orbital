09ff43cbd8bca81b881e470c1566c1ac7d62aad2
Marcelo Quesada Mergulhao <m.mergulhao@hotmail.com>
2025-09-17 20:48:13 -0300

    Started adding thrust functions based on DeltaV (WIP)

--- simulations/perturbations/thrust_perturbations.py
+++ simulations/perturbations/thrust_perturbations.py
@@ -1,7 +1,7 @@
 from utils import orbitalElements, refSystems
 import numpy as np
+import scipy
 
-orbitalElements.get_orbital_elements
 
 class FixedMassThetaIntervalThrust:
     def __init__(self, thrust, thrust_direction, m_sat, mu, angle_intervals):
@@ -22,7 +22,7 @@ class FixedMassThetaIntervalThrust:
             a_thrust = inertial_thrust_force_vector/mass
         else:
             a_thrust = 0
-        
+
         return a_thrust
 
 
@@ -58,5 +58,32 @@ class VariableMassThetaIntervalThrust:
                 a_thrust = 0
         else:
             a_thrust = 0
-        
+
+        return a_thrust
+
+
+class FixedMassDeltaVThrust:
+    def __init__(self, thrust, thrust_direction, m_sat, mu, deltaV, center_angle):
+        self.thrust = thrust
+        self.thrust_direction = thrust_direction
+        self.m_sat = m_sat
+        self.mu = mu
+        self.deltaV = deltaV
+        self.center_angle = center_angle
+
+    def get_arc_interval(self):
+
+
+    def get_acceleration(self, state_vector, dt):
+        theta = orbitalElements.get_true_anomaly(state_vector[0:3], state_vector[3:6], self.mu)
+        mass = self.m_sat
+        angle_intervals = self.angle_intervals
+
+        if any(lim1 <= theta <= lim2 for (lim1, lim2) in angle_intervals):
+            base = refSystems.get_base_versors(state_vector)
+            inertial_thrust_force_vector = self.thrust*(base @ self.thrust_direction)
+            a_thrust = inertial_thrust_force_vector/mass
+        else:
+            a_thrust = 0
+
         return a_thrust
