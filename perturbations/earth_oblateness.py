e28bc021caccb96f8a703b38fc41cfa205253c06
Marcelo <m.mergulhao@hotmail.com>
2026-09-15 00:05:47 -0300

    Removed commented code

--- perturbations/earth_oblateness.py
+++ perturbations/earth_oblateness.py
@@ -8,13 +8,6 @@ class J2_perturbation():
         r = np.linalg.norm(state_vector[0:3])
         v = np.linalg.norm(state_vector[3:6])
 
-        # pert_vec = np.array([(1 - 5*(state_vector[2]/r)**2)*(state_vector[0]/r),
-        #                      (1 - 5*(state_vector[2]/r)**2)*(state_vector[1]/r),
-        #                      (3 - 5*(state_vector[2]/r)**2)*(state_vector[2]/r)],
-        #                      dtype=np.float128)
-
-        # a_j2 = -1.5*constants.j2_earth*(constants.mu_earth/(r**2))*((constants.radius_earth/r)**2)*pert_vec
-
         pert_vec = np.array([state_vector[0]*(5*((state_vector[2]**2)/(r**2)) - 1),
                              state_vector[1]*(5*((state_vector[2]**2)/(r**2)) - 1),
                              state_vector[2]*(5*((state_vector[2]**2)/(r**2)) - 3),
